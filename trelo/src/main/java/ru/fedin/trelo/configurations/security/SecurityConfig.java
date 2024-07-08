a3578d1481b36a3d26b6250fde4dbc14023ecaa0
FadeIn64 <danik6052@gmail.com>
2024-07-08 14:46:11 +0400

    Cors disable

--- trelo/src/main/java/ru/fedin/trelo/configurations/security/SecurityConfig.java
+++ trelo/src/main/java/ru/fedin/trelo/configurations/security/SecurityConfig.java
@@ -4,8 +4,10 @@ package ru.fedin.trelo.configurations.security;
 import jakarta.persistence.ManyToMany;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -15,8 +17,11 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.authorizeHttpRequests(auth -> {
-            auth.anyRequest().permitAll();
-        });
+            auth.anyRequest()
+                    .anonymous();
+        })
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable);
         return http.build();
     }
 }
