7a4c70cc3ad76a27b29098c28f7f209bf568a553
FadeIn64 <danik6052@gmail.com>
2024-07-08 14:47:13 +0400

    Modify mappers

--- trelo/src/main/java/ru/fedin/trelo/mappers/DataMapper.java
+++ trelo/src/main/java/ru/fedin/trelo/mappers/DataMapper.java
@@ -1,6 +1,7 @@
 package ru.fedin.trelo.mappers;
 
 import java.util.Collection;
+import java.util.List;
 
 public interface DataMapper<E, D> {
 
@@ -9,4 +10,6 @@ public interface DataMapper<E, D> {
 
     Collection<E> toEntity(Collection<D> dtos);
     Collection<D> toDto(Collection<E> entities);
+    List<E> toEntity(List<D> dtos);
+    List<D> toDto(List<E> entities);
 }
