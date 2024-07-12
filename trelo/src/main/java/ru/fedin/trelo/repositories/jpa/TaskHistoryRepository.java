49d6f43be8f795d684ef8b3f8ea0cbe16a19c53a
FadeIn64 <danik6052@gmail.com>
2024-07-12 19:18:20 +0400

    Add findAllByTaskID

--- trelo/src/main/java/ru/fedin/trelo/repositories/jpa/TaskHistoryRepository.java
+++ trelo/src/main/java/ru/fedin/trelo/repositories/jpa/TaskHistoryRepository.java
@@ -1,7 +1,14 @@
 package ru.fedin.trelo.repositories.jpa;
 
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import ru.fedin.trelo.eintites.DeskTask;
 import ru.fedin.trelo.eintites.TaskHistory;
 
+import java.util.List;
+
 public interface TaskHistoryRepository extends JpaRepository<TaskHistory, Integer> {
+
+    List<TaskHistory> findAllByTask(@NotNull DeskTask task);
+
 }
--- trelo/src/main/java/ru/fedin/trelo/services/HistoryService.java
+++ trelo/src/main/java/ru/fedin/trelo/services/HistoryService.java
@@ -2,8 +2,49 @@ package ru.fedin.trelo.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.fedin.trelo.dtos.TaskHistoryDTO;
+import ru.fedin.trelo.eintites.DeskTask;
+import ru.fedin.trelo.eintites.TaskHistory;
+import ru.fedin.trelo.mappers.TaskHistoryMapper;
+import ru.fedin.trelo.repositories.jpa.DeskColumnRepository;
+import ru.fedin.trelo.repositories.jpa.TaskHistoryRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class HistoryService {
+
+    private final TaskHistoryRepository repository;
+    private final TaskHistoryMapper mapper;
+
+    private final DeskColumnRepository columnRepository;
+
+    @Transactional
+    public DeskTask changeColumn(DeskTask task, Integer newColumnId){
+
+        var opt = columnRepository.findById(newColumnId);
+        if (opt.isEmpty())
+            return task;
+        var newColumn = opt.get();
+        var oldColumn =
+                task.getColumn() == null?
+                        null
+                        : columnRepository.findById(task.getColumn()).get();
+
+        var history = TaskHistory.builder().task(task).columnTo(newColumn).columnFrom(oldColumn).build();
+        repository.save(history);
+
+        task.setColumn(newColumnId);
+        return task;
+    }
+
+    @Transactional
+    public List<TaskHistoryDTO> findAllByTask(Integer taskId){
+        var task = DeskTask.builder().id(taskId).build();
+
+        var histories = repository.findAllByTask(task);
+        return mapper.toDto(histories);
+    }
 }
