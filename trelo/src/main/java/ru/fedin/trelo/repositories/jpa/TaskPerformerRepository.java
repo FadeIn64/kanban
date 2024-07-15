f83f9ea356a0aa05123002091a2554c8fd31f8c6
FadeIn64 <danik6052@gmail.com>
2024-07-15 16:37:38 +0400

    Add findByContributor

--- trelo/src/main/java/ru/fedin/trelo/repositories/jpa/TaskPerformerRepository.java
+++ trelo/src/main/java/ru/fedin/trelo/repositories/jpa/TaskPerformerRepository.java
@@ -1,7 +1,12 @@
 package ru.fedin.trelo.repositories.jpa;
 
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import ru.fedin.trelo.eintites.DeskContributor;
 import ru.fedin.trelo.eintites.TaskPerformer;
 
+import java.util.Optional;
+
 public interface TaskPerformerRepository extends JpaRepository<TaskPerformer, Integer> {
+    Optional<TaskPerformer> findByContributor(@NotNull DeskContributor contributor);
 }
