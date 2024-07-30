78027dc3632cd89064f8373ba45828339718691e
FadeIn64 <danik6052@gmail.com>
2024-07-30 19:37:09 +0400

    add kafka non-reply config

--- trelo-client/src/main/java/ru/fedin/treloclient/configurations/kafka/KafkaClientConfiguration.java
+++ trelo-client/src/main/java/ru/fedin/treloclient/configurations/kafka/KafkaClientConfiguration.java
@@ -9,7 +9,9 @@ import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
+import ru.fedin.treloclient.dtos.requests.DeskColumnReq;
 import ru.fedin.treloclient.dtos.requests.DeskReq;
+import ru.fedin.treloclient.dtos.requests.DeskTaskReq;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,21 +23,54 @@ public class KafkaClientConfiguration {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
-    @Value("${kafka.topic.desk}")
-    private String deskTopic;
+    @Configuration
+    public class DeskClientConfiguration{
+        public ProducerFactory<UUID, DeskReq> deskProducerFactory() {
+            Map<String, Object> configProps = new HashMap<>();
+            configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+            configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, UUIDSerializer.class);
+            configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+            return new DefaultKafkaProducerFactory<>(configProps);
+        }
 
-
-    public ProducerFactory<UUID, DeskReq> orderProducerFactory() {
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, UUIDSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        return new DefaultKafkaProducerFactory<>(configProps);
+        @Bean("DeskTemplate")
+        public KafkaTemplate<UUID, DeskReq> deskKafkaTemplate() {
+            return new KafkaTemplate<>(deskProducerFactory());
+        }
     }
 
-    @Bean("DeskTemplate")
-    public KafkaTemplate<UUID, DeskReq> deskKafkaTemplate() {
-        return new KafkaTemplate<>(orderProducerFactory());
+    @Configuration
+    public class ColumnClientConfiguration{
+        public ProducerFactory<UUID, DeskColumnReq> columnProducerFactory() {
+            Map<String, Object> configProps = new HashMap<>();
+            configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+            configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, UUIDSerializer.class);
+            configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+            return new DefaultKafkaProducerFactory<>(configProps);
+        }
+
+        @Bean("ColumnTemplate")
+        public KafkaTemplate<UUID, DeskColumnReq> columnkKafkaTemplate() {
+            return new KafkaTemplate<>(columnProducerFactory());
+        }
     }
+
+    @Configuration
+    public class TaskClientConfiguration{
+        public ProducerFactory<UUID, DeskTaskReq> taskProducerFactory() {
+            Map<String, Object> configProps = new HashMap<>();
+            configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+            configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, UUIDSerializer.class);
+            configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+            return new DefaultKafkaProducerFactory<>(configProps);
+        }
+
+        @Bean("TaskTemplate")
+        public KafkaTemplate<UUID, DeskTaskReq> taskKafkaTemplate() {
+            return new KafkaTemplate<>(taskProducerFactory());
+        }
+    }
+
+
 
 }
--- trelo/src/main/java/ru/fedin/trelo/configurations/kafka/KafkaServerConfiguration.java
+++ trelo/src/main/java/ru/fedin/trelo/configurations/kafka/KafkaServerConfiguration.java
@@ -3,7 +3,6 @@ package ru.fedin.trelo.configurations.kafka;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.UUIDDeserializer;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +12,9 @@ import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
+import ru.fedin.trelo.dtos.DeskColumnDTO;
 import ru.fedin.trelo.dtos.DeskDTO;
+import ru.fedin.trelo.dtos.DeskTaskDTO;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -26,37 +27,108 @@ public class KafkaServerConfiguration {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
-    @Value("${kafka.topic.desk}")
-    private String deskTopic;
+    @Configuration
+    public class DeskConfiguration{
+        @Value("${kafka.topic.desk}")
+        private String deskTopic;
 
 
-    @Bean
-    NewTopic desk(){
-        return TopicBuilder.name(deskTopic)
-                .partitions(10)
-                .build();
+        @Bean
+        public NewTopic desk(){
+            return TopicBuilder.name(deskTopic)
+                    .partitions(10)
+                    .build();
+        }
+
+        private ConsumerFactory<UUID, DeskDTO> deskConsumerFactory() {
+            Map<String, Object> props = new HashMap<>();
+            props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+            return new DefaultKafkaConsumerFactory<>(props, new UUIDDeserializer(), jsonDeserializer());
+        }
+
+        private JsonDeserializer<DeskDTO> jsonDeserializer(){
+            JsonDeserializer<DeskDTO> deserializer = new JsonDeserializer<>(DeskDTO.class);
+            deserializer.addTrustedPackages("*");
+            deserializer.ignoreTypeHeaders();
+            return deserializer;
+        }
+
+        @Bean
+        public ConcurrentKafkaListenerContainerFactory<UUID, DeskDTO> deskKafkaListenerContainerFactory() {
+            ConcurrentKafkaListenerContainerFactory<UUID, DeskDTO> factory = new ConcurrentKafkaListenerContainerFactory<>();
+            factory.setConsumerFactory(deskConsumerFactory());
+            return factory;
+        }
     }
 
-    private ConsumerFactory<UUID, DeskDTO> deskConsumerFactory() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        return new DefaultKafkaConsumerFactory<>(props, new UUIDDeserializer(), jsonDeserializer());
+
+    @Configuration
+    public class ColumnConfiguration{
+        @Value("${kafka.topic.column}")
+        private String columnTopic;
+
+
+        @Bean
+        public NewTopic column(){
+            return TopicBuilder.name(columnTopic)
+                    .partitions(10)
+                    .build();
+        }
+
+        private ConsumerFactory<UUID, DeskColumnDTO> columnConsumerFactory() {
+            Map<String, Object> props = new HashMap<>();
+            props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+            return new DefaultKafkaConsumerFactory<>(props, new UUIDDeserializer(), jsonDeserializer());
+        }
+
+        private JsonDeserializer<DeskColumnDTO> jsonDeserializer(){
+            JsonDeserializer<DeskColumnDTO> deserializer = new JsonDeserializer<>(DeskColumnDTO.class);
+            deserializer.addTrustedPackages("*");
+            deserializer.ignoreTypeHeaders();
+            return deserializer;
+        }
+
+        @Bean
+        public ConcurrentKafkaListenerContainerFactory<UUID, DeskColumnDTO> columnKafkaListenerContainerFactory() {
+            ConcurrentKafkaListenerContainerFactory<UUID, DeskColumnDTO> factory = new ConcurrentKafkaListenerContainerFactory<>();
+            factory.setConsumerFactory(columnConsumerFactory());
+            return factory;
+        }
     }
 
-    private JsonDeserializer<DeskDTO> jsonDeserializer(){
-        JsonDeserializer<DeskDTO> deserializer = new JsonDeserializer<>(DeskDTO.class);
-        deserializer.addTrustedPackages("*");
-        deserializer.ignoreTypeHeaders();
-        return deserializer;
+
+    @Configuration
+    public class TaskConfiguration{
+        @Value("${kafka.topic.task}")
+        private String taskTopic;
+
+
+        @Bean
+        public NewTopic task(){
+            return TopicBuilder.name(taskTopic)
+                    .partitions(10)
+                    .build();
+        }
+
+        private ConsumerFactory<UUID, DeskTaskDTO> taskConsumerFactory() {
+            Map<String, Object> props = new HashMap<>();
+            props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+            return new DefaultKafkaConsumerFactory<>(props, new UUIDDeserializer(), jsonDeserializer());
+        }
+
+        private JsonDeserializer<DeskTaskDTO> jsonDeserializer(){
+            JsonDeserializer<DeskTaskDTO> deserializer = new JsonDeserializer<>(DeskTaskDTO.class);
+            deserializer.addTrustedPackages("*");
+            deserializer.ignoreTypeHeaders();
+            return deserializer;
+        }
+
+        @Bean
+        public ConcurrentKafkaListenerContainerFactory<UUID, DeskTaskDTO> taskKafkaListenerContainerFactory() {
+            ConcurrentKafkaListenerContainerFactory<UUID, DeskTaskDTO> factory = new ConcurrentKafkaListenerContainerFactory<>();
+            factory.setConsumerFactory(taskConsumerFactory());
+            return factory;
+        }
     }
-
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<UUID, DeskDTO> deskKafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<UUID, DeskDTO> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(deskConsumerFactory());
-        return factory;
-    }
-
-
 
 }
