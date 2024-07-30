af1b01d952d35f6c82ee710f72fb7dfbb15576d4
FadeIn64 <danik6052@gmail.com>
2024-07-30 16:04:41 +0400

    add get methods in desk service

--- trelo-client/src/main/java/ru/fedin/treloclient/services/DeskService.java
+++ trelo-client/src/main/java/ru/fedin/treloclient/services/DeskService.java
@@ -2,11 +2,13 @@ package ru.fedin.treloclient.services;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestClient;
 import ru.fedin.treloclient.dtos.requests.DeskContributorReq;
 import ru.fedin.treloclient.dtos.requests.DeskReq;
+import ru.fedin.treloclient.dtos.response.DeskRes;
 
 
 import java.util.ArrayList;
@@ -19,37 +21,37 @@ public class DeskService {
     private final RestClient restClient;
 
 
-    public DeskReq findById(int id){
-        DeskReq req = restClient.get()
-                .uri("/desk/"+id).retrieve()
-                .body(DeskReq.class);
-        return req;
+    public DeskRes findById(int id){
+        ResponseEntity<DeskRes> res = restClient.get()
+                .uri("/desk/"+id).retrieve().toEntity(DeskRes.class);
+        var entity = res.getBody();
+        return entity;
     }
 
-    @Transactional
+
     public DeskReq create(DeskReq dto){
 
         return dto;
     }
 
-    @Transactional
+
     public void delete(Integer id){
 
     }
 
-    @Transactional
+
     public DeskReq rename(Integer deskId, String newName){
 
         return DeskReq.builder().id(deskId).build();
     }
 
-    @Transactional
+
     public List<DeskContributorReq> addContributor(int deskId, String user){
 
         return new ArrayList<>();
     }
 
-    @Transactional
+
     public boolean removeContributor(int deskId, String user){
 
         return true;
