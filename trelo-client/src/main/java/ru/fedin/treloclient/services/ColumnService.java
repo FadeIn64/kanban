0dda17da6c4831647db8dcc35aa78c8bc2b29098
FadeIn64 <danik6052@gmail.com>
2024-07-30 16:12:54 +0400

    add get methods in column service

--- trelo-client/src/main/java/ru/fedin/treloclient/services/ColumnService.java
+++ trelo-client/src/main/java/ru/fedin/treloclient/services/ColumnService.java
@@ -3,7 +3,9 @@ package ru.fedin.treloclient.services;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestClient;
 import ru.fedin.treloclient.dtos.requests.DeskColumnReq;
+import ru.fedin.treloclient.dtos.response.DeskColumnRes;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,25 +15,35 @@ import java.util.List;
 public class ColumnService {
 
 
+    private final RestClient restClient;
 
 
-    public DeskColumnReq findById(Integer id){
 
-        return DeskColumnReq.builder().id(id).build();
+    public DeskColumnRes findById(Integer id){
+
+        var res = restClient
+                .get()
+                .uri("/column/" + id)
+                .retrieve()
+                .toEntity(DeskColumnRes.class);
+
+        var entity = res.getBody();
+
+        return entity;
     }
 
-    @Transactional
+
     public DeskColumnReq create(DeskColumnReq dto){
 
         return dto;
     }
 
-    @Transactional
+
     public DeskColumnReq rename(Integer id, String newName){
        return DeskColumnReq.builder().id(id).build();
     }
 
-    @Transactional
+
     public List<DeskColumnReq> move(Integer id, int offset){
 
         return new ArrayList<>();
