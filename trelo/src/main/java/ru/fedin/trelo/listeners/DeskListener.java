bc94c1d78694a6a1b4147de6b968b375d44c5de0
FadeIn64 <danik6052@gmail.com>
2024-07-31 19:16:35 +0400

    add create desk method

--- trelo/src/main/java/ru/fedin/trelo/listeners/DeskListener.java
+++ trelo/src/main/java/ru/fedin/trelo/listeners/DeskListener.java
@@ -1,20 +1,28 @@
 package ru.fedin.trelo.listeners;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import ru.fedin.trelo.dtos.DeskDTO;
+import ru.fedin.trelo.services.DeskService;
 
 import java.util.UUID;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class DeskListener {
+
+    private final DeskService deskService;
+
     @KafkaListener(topics = "${kafka.topic.desk}",
                     groupId = "server",
                     containerFactory = "deskKafkaListenerContainerFactory")
     void listener(DeskDTO dto){
-        log.info("Desk: {}", dto);
+        if (dto.getId() == null){
+            dto = deskService.create(dto);
+        }
     }
 
 }
