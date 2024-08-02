ccafc6217e1ab7b0d95ad87d309658f1cdccd365
FadeIn64 <danik6052@gmail.com>
2024-08-02 17:59:39 +0400

    add DeskCache update

--- trelo-client/src/main/java/ru/fedin/treloclient/listeners/DeskListener.java
+++ trelo-client/src/main/java/ru/fedin/treloclient/listeners/DeskListener.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import ru.fedin.treloclient.cache.DeskCacheService;
 import ru.fedin.treloclient.dtos.response.DeskRes;
 
 @Component
@@ -11,11 +12,18 @@ import ru.fedin.treloclient.dtos.response.DeskRes;
 @RequiredArgsConstructor
 public class DeskListener {
 
+    private final DeskCacheService cacheService;
+
     @KafkaListener(topics = "#{replyDeskTopic}",
                     groupId = "desk",
                     containerFactory = "replyDeskKafkaListenerContainerFactory")
     void listener( DeskRes desk){
         log.info("Desk: {}", desk);
+        if ("".equals(desk.getName())){
+            cacheService.delete(desk.getId());
+            return;
+        }
+        cacheService.save(desk);
     }
 
 }
