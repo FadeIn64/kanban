481459764124e383da62e5d0f2b92bbb37ae5dd9
FadeIn64 <danik6052@gmail.com>
2024-08-02 16:05:38 +0400

    add reply sending to columns

--- trelo/src/main/java/ru/fedin/trelo/listeners/ColumnListener.java
+++ trelo/src/main/java/ru/fedin/trelo/listeners/ColumnListener.java
@@ -1,12 +1,16 @@
 package ru.fedin.trelo.listeners;
 
+import com.fasterxml.uuid.Generators;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 import ru.fedin.trelo.dtos.kafka.DeskColumnRes;
 import ru.fedin.trelo.dtos.kafka.DeskRes;
 import ru.fedin.trelo.mappers.kafka.ColumnMapperKafka;
 import ru.fedin.trelo.services.ColumnService;
+
+import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
@@ -14,6 +18,8 @@ public class ColumnListener {
 
     private final ColumnService columnService;
     private final ColumnMapperKafka columnMapper;
+    private final KafkaTemplate<UUID, DeskColumnRes> replyColumnTemplate;
+    private final String replyColumnTopic;
 
 
     @KafkaListener(topics = "${kafka.topic.column}",
@@ -23,11 +29,13 @@ public class ColumnListener {
 
         if(column.getId() == 0){
             column = columnMapper.toEntity(columnService.create(columnMapper.toDto(column)));
+            send(column);
             return;
         }
 
         if ("".equals(column.getName())){
             columnService.removeColumn(column.getId());
+            send(column);
             return;
         }
 
@@ -38,6 +46,7 @@ public class ColumnListener {
 
         if (!dto.getName().equals(column.getName())){
             column = columnMapper.toEntity(columnService.rename(column.getId(), column.getName()));
+            send(column);
             return;
         }
 
@@ -51,6 +60,11 @@ public class ColumnListener {
                         .filter(c -> c.getId().equals(dto.getId()))
                         .findFirst().orElse(dto)
                 );
+        send(column);
 
+    }
+
+    private void send(DeskColumnRes column){
+        replyColumnTemplate.send(replyColumnTopic, Generators.timeBasedGenerator().generate(), column);
     }
 }
