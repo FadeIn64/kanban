7099dcb8fb13438839201d2855585fb436850df9
FadeIn64 <danik6052@gmail.com>
2024-08-03 16:31:25 +0400

    change task repository to search available

--- trelo/src/main/java/ru/fedin/trelo/repositories/jpa/DeskTaskRepository.java
+++ trelo/src/main/java/ru/fedin/trelo/repositories/jpa/DeskTaskRepository.java
@@ -1,10 +1,11 @@
 package ru.fedin.trelo.repositories.jpa;
 
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import ru.fedin.trelo.eintites.DeskTask;
 
 import java.util.List;
 
-public interface DeskTaskRepository extends DeleteModifyJpaRepository<DeskTask, Integer> {
+public interface DeskTaskRepository extends DeleteModifyJpaRepository<DeskTask, Integer>, JpaSpecificationExecutor<DeskTask> {
 
     List<DeskTask> findAllByColumn(Integer column);
 }
