877732390e924eec98061b0597780ffeb3d21799
FadeIn64 <danik6052@gmail.com>
2024-08-05 16:34:57 +0400

    create data interval check

---
+++ trelo/src/main/java/ru/fedin/trelo/validation/DataIntervalCheck.java
@@ -0,0 +1,32 @@
+package ru.fedin.trelo.validation;
+
+
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = DataIntervalValidator.class)
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DataIntervalCheck {
+
+    String message() default "Data incorrect";
+
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+
+    String startDate();
+
+    String endDate();
+
+    @Target({ ElementType.TYPE })
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface List {
+        DataIntervalCheck[] value();
+    }
+}
