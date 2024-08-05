c1d0ce933f39cf5e65950563e5822cfae905a76f
FadeIn64 <danik6052@gmail.com>
2024-08-05 16:59:05 +0400

    add search in client

---
+++ trelo-client/src/main/java/ru/fedin/treloclient/search/FieldType.java
@@ -0,0 +1,10 @@
+package ru.fedin.treloclient.search;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public enum FieldType {
+
+    BOOLEAN, CHAR, DATE, DOUBLE, INTEGER, LONG, STRING
+
+}
---
+++ trelo-client/src/main/java/ru/fedin/treloclient/search/SortDirection.java
@@ -0,0 +1,7 @@
+package ru.fedin.treloclient.search;
+
+
+public enum SortDirection {
+
+    ASC, DESC
+}
