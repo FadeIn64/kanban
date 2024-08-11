bfdcd797ad8eb6951e76016e9628ca77585e88de
FadeIn64 <danik6052@gmail.com>
2024-08-11 19:46:40 +0400

    add refactor project

---
+++ trelo-refactor/src/main/java/ru/fedin/trelorefactor/TreloRefactorApplication.java
@@ -0,0 +1,13 @@
+package ru.fedin.trelorefactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreloRefactorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreloRefactorApplication.class, args);
+    }
+
+}
---
+++ trelo-refactor/src/test/java/ru/fedin/trelorefactor/TreloRefactorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fedin.trelorefactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreloRefactorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
