50b25aa449170e9191b54109fabd184db9b31938
FadeIn64 <danik6052@gmail.com>
2024-08-14 17:06:39 +0400

    change exception

--- trelo-refactor/src/main/java/ru/fedin/trelorefactor/exceptions/UpdateOrInsertException.java
+++ trelo-refactor/src/main/java/ru/fedin/trelorefactor/exceptions/UpdateOrInsertException.java
@@ -1,5 +1,8 @@
 package ru.fedin.trelorefactor.exceptions;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class UpdateOrInsertException extends RuntimeException{
 
     public UpdateOrInsertException() {
@@ -11,13 +14,16 @@ public class UpdateOrInsertException extends RuntimeException{
 
     public UpdateOrInsertException(String message, Throwable cause) {
         super(message, cause);
+        log.error("Error: ", cause);
     }
 
     public UpdateOrInsertException(Throwable cause) {
         super(cause);
+        log.error("Error: ", cause);
     }
 
     public UpdateOrInsertException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
+        log.error("Error: ", cause);
     }
 }
