7e37326407511caf578fcf8b820fc086db14fb54
FadeIn64 <danik6052@gmail.com>
2024-08-21 15:12:13 +0400

    add TaskListener

---
+++ trelo-refactor/src/main/java/ru/fedin/trelorefactor/listeners/TaskListener.java
@@ -0,0 +1,60 @@
+package ru.fedin.trelorefactor.listeners;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+import ru.fedin.trelorefactor.exceptions.ModifyDataException;
+import ru.fedin.trelorefactor.mappers.models.TaskModelMapper;
+import ru.fedin.trelorefactor.messaging.*;
+import ru.fedin.trelorefactor.models.TaskModel;
+import ru.fedin.trelorefactor.services.TaskService;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class TaskListener {
+
+    private final TaskService taskService;
+    private final TaskModelMapper taskMapper;
+
+    @KafkaListener(topics = "${kafka.topic.task}",
+            groupId = "server",
+            containerFactory = "deskKafkaListenerContainerFactory")
+    public void listener( Message<TaskModel, TaskAction> message){
+        log.info("Received Task message: {}", message);
+
+
+        Message<TaskModel, TaskAction> reply = new Message<>();
+        if (message.getAction().name().equals(DeskAction.REMOVE.name())){
+            reply.setAction(TaskAction.REMOVE);
+        }
+        else {
+            reply.setAction(TaskAction.CACHE);
+        }
+
+        if (message.getMessage().getId() == null || 0L == message.getMessage().getId()){
+            reply.setStatus(new MessageStatus(Status.ERROR, "id equals null"));
+            reply.setMessage(message.getMessage());
+            return;
+        }
+
+        try {
+            reply.setMessage(taskMapper.toEntity(message.getAction().action(taskMapper.toDto(message.getMessage()), taskService)));
+            reply.setStatus(new MessageStatus(Status.OK, "Ok"));
+        }
+        catch (ModifyDataException e){
+            log.error("ModifyDataException", e);
+            reply.setMessage(message.getMessage());
+            reply.setStatus(new MessageStatus(Status.BAD_REQUEST, "ModifyDataException: " + e.getMessage()));
+        }
+        catch (Exception e){
+            log.error("Exception", e);
+            reply.setStatus(new MessageStatus(Status.ERROR, e.getClass().getName() + ": " + e.getMessage()));
+        }
+
+        log.info("Reply Task received: {}", reply);
+
+    }
+
+}
