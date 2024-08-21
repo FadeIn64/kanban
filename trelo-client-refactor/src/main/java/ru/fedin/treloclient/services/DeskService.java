3fec020cd14d21bc47d0ce2cb130fb71374e060e
FadeIn64 <danik6052@gmail.com>
2024-08-21 17:00:38 +0400

    add DeskService

---
+++ trelo-client-refactor/src/main/java/ru/fedin/treloclient/services/DeskService.java
@@ -0,0 +1,76 @@
+package ru.fedin.treloclient.services;
+
+import com.fasterxml.uuid.Generators;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+import ru.fedin.treloclient.dtos.DeskDto;
+import ru.fedin.treloclient.mappers.models.DeskModelMapper;
+import ru.fedin.treloclient.messaging.DeskAction;
+import ru.fedin.treloclient.messaging.Message;
+import ru.fedin.treloclient.messaging.MessageStatus;
+import ru.fedin.treloclient.messaging.Status;
+import ru.fedin.treloclient.models.DeskModel;
+import ru.fedin.treloclient.models.UserModel;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class DeskService {
+
+    private final RestClient restClient;
+    private final KafkaTemplate<UUID, Message<DeskModel, DeskAction>> deskTemplate;
+    private final DeskModelMapper deskMapper;
+
+    public DeskDto findById(long deskId) {
+        return deskMapper.toDto(findModelById(deskId));
+    }
+
+    private DeskModel findModelById(long deskId) {
+        return restClient
+                .get()
+                .uri(String.format("/desk/%d", deskId))
+                .retrieve()
+                .body(DeskModel.class);
+    }
+
+    public void create(DeskDto desk) {
+        DeskModel model = deskMapper.toEntity(desk);
+        send(model, DeskAction.CREATE);
+    }
+
+    public void delete(long deskId) {
+        DeskModel model = findModelById(deskId);
+        send(model, DeskAction.REMOVE);
+    }
+
+    public void rename(long deskId, String newName) {
+        DeskModel model = findModelById(deskId);
+        model.setName(newName);
+        send(model, DeskAction.RENAME);
+    }
+
+    public void addContributor(long deskId, long user) {
+        DeskModel model = findModelById(deskId);
+        model.setUsers(List.of(UserModel.builder().id(user).build()));
+        send(model, DeskAction.ADD_CONTRIBUTOR);
+    }
+
+    public void removeContributor(long deskId, long user) {
+        DeskModel model = findModelById(deskId);
+        model.setUsers(List.of(UserModel.builder().id(user).build()));
+        send(model, DeskAction.REMOVE_CONTRIBUTOR);
+    }
+
+    private void send(DeskModel desk, DeskAction action) {
+        Message<DeskModel, DeskAction> message = new Message<>();
+        message.setAction(action);
+        message.setMessage(desk);
+        message.setStatus(new MessageStatus(Status.OK, "no reason"));
+        UUID uuid = Generators.timeBasedGenerator().generate();
+        deskTemplate.sendDefault(uuid, message);
+    }
+}
