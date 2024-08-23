adc606e2995db5ffc37fcc1501c8d83e8a48554c
FadeIn64 <danik6052@gmail.com>
2024-08-23 15:12:20 +0400

    add File Mapper

--- trelo-refactor/src/main/java/ru/fedin/trelorefactor/mappers/entities/FileMapper.java
+++ trelo-refactor/src/main/java/ru/fedin/trelorefactor/mappers/entities/FileMapper.java
@@ -1,8 +1,10 @@
 package ru.fedin.trelorefactor.mappers.entities;
 
+import org.mapstruct.Mapper;
 import ru.fedin.trelorefactor.dtos.minio.FileDto;
 import ru.fedin.trelorefactor.eintites.File;
 import ru.fedin.trelorefactor.mappers.DataMapper;
 
+@Mapper(componentModel = "spring")
 public interface FileMapper extends DataMapper<File, FileDto> {
 }
